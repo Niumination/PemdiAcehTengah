605fb1b3dc01c14729819aab9e48e8309e7e485e
Niumination <niumination@gmail.com>
2026-06-13 17:17:56 +0700

    chore: cleanup temp verify script

--- _verify_glosarium.py
+++
@@ -1,7 +0,0 @@
-import json
-d = json.load(open("data/glosarium.json"))
-print(f"Jumlah entri: {len(d)}")
-for x in d[:5]:
-    s = len(x.get("singkat", ""))
-    l = len(x.get("lengkap", ""))
-    print(f"  - {x['istilah']}: singkat={s} chars, lengkap={l} chars")
